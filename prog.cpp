21b88b93b9a199ef413097723cb4ea0e45b6ecd6
LordLoles <30018846+LordLoles@users.noreply.github.com>
2020-10-25 22:45:42 +0100

    nakodene vytvaranie cesty s danymi zoznamami. treba vsetky kombinacie otestovat na nerepetitivnost

--- prog.cpp
+++ prog.cpp
@@ -1,16 +1,110 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <tuple>
+#include <algorithm>
 
 using namespace std;
 
+const int length = 2;
+const int colors = 4;
+
+
+vector<tuple<int, int, int>> getSubsetsOfLength3(int set[], int setLength)
+{
+    vector<tuple<int, int, int>> res;
+    int count = 0; //for testing
+    for (int i = 0; i < setLength; i++){
+        for (int j = i+1; j < setLength; j++){
+            for (int k = j+1; k < setLength; k++){
+                res.push_back({i, j, k});
+                count++;
+            }
+        }
+    }
+    cout << "subsets count = " << count << endl;
+    return res;
+}
+
+int* getSetToLength(){
+    int* numbers = new int[colors];
+    for(int i = 0; i < colors; i++){
+        numbers[i] = i;
+    }
+    return numbers;
+}
+
+void printVectorTuple(vector<tuple<int, int, int>> vt)
+{
+    for (tuple<int, int, int> &list : vt)
+        cout << "[" << get<0>(list) << " "<< get<1>(list) << " "<< get<2>(list) << "] ";
+    cout << endl;
+}
+
+vector<int> decimalToBase(int decimal, int base)
+{
+    vector<int> res;
+    while (decimal > 0){
+        res.push_back(decimal % base);
+        decimal /= base;
+    }
+    reverse(res.begin(), res.end());
+    return res;
+}
+
+vector<int> vectorToLength(vector<int> a, int length)
+{
+    if (a.size() == length) return a;
+    if (a.size() > length) return vector<int>();
+    vector<int> res = a;
+    reverse(res.begin(), res.end());
+    for (int i = res.size(); i < length; i++) res.push_back(0);
+    reverse(res.begin(), res.end());
+    return res;
+}
+
+void printVector(vector <int> const &a)
+{
+    for(int i=0; i < a.size(); i++)
+        cout << a.at(i) << ' ';
+    cout << endl;
+}
+
+vector<tuple<int, int, int>> allColorSubsets = getSubsetsOfLength3(getSetToLength(), colors); //all possible lists of colors (triplets) that will be used on verteces
+long long nextLists = 0; //next indeces of color lists (allColorSubsets) to be used in nextPath() in decimal
+
+vector<tuple<int, int, int>> nextPath()
+{
+    vector<tuple<int, int, int>> res;
+    vector<int> toFind = vectorToLength(decimalToBase(nextLists, allColorSubsets.size()), length);
+    if (toFind.empty()) return vector<tuple<int, int, int>>();
+    for (int i = 0; i < length; i++){
+        res.push_back(allColorSubsets[toFind[i]]);
+    }
+    nextLists++;
+    return res;
+}
+
+vector<int> getColoring()
+{
+    asi tak isto (podobne), ako nextpath()
+}
+
+
 int main()
 {
-    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+    cout << "all subsets: ";
+    printVectorTuple(allColorSubsets);
 
-    for (const string& word : msg)
+    vector<tuple<int, int, int>> nowPath = nextPath();
+
+    while (!nowPath.empty())
     {
-        cout << word << " ";
+        printVectorTuple(nowPath);
+
+
+
+        nowPath = nextPath();
     }
-    cout << endl;
+
 }
